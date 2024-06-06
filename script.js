0d26b2c55230157a954cea4ff887aec31da7e211
Warren <the.waz88@gmail.com>
2024-06-06 16:18:09 +0200

    Added button event listener and div creation algorithm based on prompt input

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const container = document.getElementById("container");
+const startButton = document.getElementById("startButton")
+
+let grid;
+
+startButton.addEventListener('click', () => {
+    grid = Math.min(gridSize(), 100);
+    for (let i = 0; i < grid; i++) {
+        for (let j = 0; j < grid; j++) {
+            const block = document.createElement("div")
+            block.classList.add("block-item")
+            container.appendChild(block)
+        }
+    }
+})
 
 function gridSize() {
     return prompt("What size would you like to draw on? 1 - 100", "16");
