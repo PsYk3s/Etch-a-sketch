52b130837ddd02f585b13256816976e8709af451
Warren <the.waz88@gmail.com>
2024-06-20 15:14:26 +0200

    Refactored to use single loop for generating grid

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const startButton = document.getElementById("startButton")
 
 let grid;
 
+//Create grid using loops
 startButton.addEventListener('click', () => {
     container.innerText = ""
+
+    //Retrieve user grid size at a maximum of 100
     grid = Math.min(gridSize(), 100);
-    for (let i = 0; i < grid; i++) {
-        for (let j = 0; j < grid; j++) {
-            const block = document.createElement("div")
-            block.classList.add("block-item")
-            container.appendChild(block)
-            block.style.width = "calc(100% /" + grid + ")"
-        }
+
+    for (let i = 0; i < grid * grid; i++) {
+        const block = document.createElement("div")
+        block.classList.add("block-item")
+        container.appendChild(block)
+        block.style.width = "calc(100% /" + grid + ")"
     }
+
     startButton.innerText = "Reset?"
 })
 
